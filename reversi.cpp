bb3e7e51e6dd76085f5739b3595863a8e3d33d5c
TANIGUCHI Masaya <ta2gch@gmail.com>
2022-06-02 13:04:21 +0000

    Use preprocessor macro

--- reversi.cpp
+++ reversi.cpp
@@ -12,17 +12,31 @@
 
 using Board = uint64_t;
 
-// C++20 std::bit_floor
-uint64_t bit_floor(uint64_t x) {
-  return 1ull << (63 - __builtin_clzll(x));
-  //return 1ull << static_cast<size_t>(std::log2(x));
-}
-
-// C++20 std::popcount
-size_t popcount(uint64_t x) {
-  return __builtin_popcountll(x);
-  //return std::bitset<64>(x).count();
-}
+// C++20 std::bit_floor std::popcount
+#if defined(__cplusplus) && __cplusplus >= 202002L
+  uint64_t bit_floor(uint64_t x) {
+    return std::bit_floor(x);
+  }
+  size_t popcount(uint64_t x) {
+    return std::popcount(x);
+  }
+#elif defined(__clang__) || defined(__GNUC__)
+  uint64_t bit_floor(uint64_t x) {
+    return 1ull << (64 - __builtin_clzll(x) - 1);
+  }
+  size_t popcount(uint64_t x) {
+    return __builtin_popcountll(x);
+  }
+#else
+#include<bitset>
+#include<cmath>
+  uint64_t bit_floor(uint64_t x) {
+    return 1ull << static_cast<size_t>(std::log2(x));
+  }
+  size_t popcount(uint64_t x) {
+    return std::bitset<64>(x).count();
+  }
+#endif
 
 Board get_position(size_t x, size_t y) {
   return 1ull << (x + y * 8);
