3fd57bbf19c93b4f3cc02f229fc1e57e8208453c
TANIGUCHI Masaya <ta2gch@gmail.com>
2022-06-05 15:20:18 +0000

    Add TUI

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include<cassert>
 #include<cstdio>
 #include<cmath>
+#include <cstdlib>
+#include<iostream>
 
 #include"./board.hpp"
 #include"./string.hpp"
@@ -11,9 +13,10 @@
 #include "candidates.hpp"
 #include "reverse.hpp"
 
-constexpr Board get_position(size_t x, size_t y) {
-  return 1ull << 63 >> (x + y * 8);
+constexpr Board get_position(char x, char y) {
+  return 1ull << 63 >> ((x - 'a') + (y - '1') * 8);
 }
+
 TEST_BEGIN(get_position)
 const Board expected = 
   board(
@@ -26,7 +29,7 @@ const Board expected =
     0b00000000ull,
     0b00000000ull
   );
-static_assert(expected == get_position(2, 3));
+static_assert(expected == get_position('c', '4'));
 TEST_END(get_position)
 
 Board search(Board black, Board white, Eval evaluate) {
@@ -35,7 +38,6 @@ Board search(Board black, Board white, Eval evaluate) {
   Board best_position = 0ull;
   double best_score = 0;
   while((position = bit_floor(candidates))) {
-    printf("%ld\n", position);
     Board reverse = get_reverse(white, black, position);
     Board black1 = black ^ reverse;
     Board white1 = white ^ reverse ^ position;
@@ -58,22 +60,50 @@ int main(int argc, char** argv) {
     "        "
     "        "
   );
-  auto [white1, black1] = simulate(white, black, 20);
-  auto candidates = get_candidates(black1, white1);
-  Board position = bit_floor(candidates);
-  Board reverse = get_reverse(black, white, position);
-  black1 ^= reverse ^ position;
-  white1 ^= reverse;
+  print(black, white);
+  Board position, reverse;
+  char x, y;
+  while(true) {
+    printf("(%2ld/%2ld) YOU> ", popcount(black), popcount(white));
+    std::cin >> x >> y;
+    if (x == 'q' || y == 'q') {
+      exit(0);
+    }
+    position = get_position(x, y);
+    reverse = get_reverse(black, white, position);
+    black ^= reverse ^ position;
+    white ^= reverse;
+    print(black, white);
+    position = search(black, white, [](auto black, auto white){ return evaluate_monte_carlo_random(black, white);});
+    if (position) {
+      x = (int)std::log2(position) % 8;
+      y = (int)std::log2(position) / 8;
+      printf("(%2ld/%2ld) COM> %c%c\n", popcount(black), popcount(white), (int)std::log2(position) % 8 + 'a', y + '1');
+      reverse = get_reverse(white, black, position);
+      black ^= reverse;
+      white ^= reverse ^ position;
+      print(black, white);
+    } else {
+      printf("(%2ld/%2ld) COM> PASS\n", popcount(black), popcount(white));
+      print(black, white);
+    }
+  }
+  //auto [white1, black1] = simulate(white, black, 20);
+  //auto candidates = get_candidates(black1, white1);
+  //Board position = bit_floor(candidates);
+  //Board reverse = get_reverse(black, white, position);
+  //black1 ^= reverse ^ position;
+  //white1 ^= reverse;
 
-  Board best_position = search(black, white, [](auto black, auto white){
-    return evaluate_nega_alpha(black, white, [](auto black, auto white){
-      return evaluate_monte_carlo_random(black, white, 10);
-    }, 6);
-  });
-  std::puts(stringify(best_position, 0ull, true).c_str());
-  std::printf(" NegaAlphaRate: %lg\n", evaluate_nega_alpha(black1, white1, [](auto black, auto white){return evaluate_monte_carlo_full(black, white);}));
-  std::printf(" NegaAlphaRate: %lg\n", evaluate_nega_alpha(black1, white1, [](auto black, auto white){return evaluate_static(black, white);}));
-  std::printf(" NegaAlphaRate: %lg\n", evaluate_nega_alpha(black1, white1, [](auto black, auto white){return evaluate_win_loss(black, white);}, 10));
-  std::printf("MonteCarloRate: %lg\n", evaluate_monte_carlo_random(black1, white1));
+  //Board best_position = search(black, white, [](auto black, auto white){
+  //  return evaluate_nega_alpha(black, white, [](auto black, auto white){
+  //    return evaluate_monte_carlo_random(black, white, 1000);
+  //  }, 2);
+  //});
+  //print(best_position, 0ull);
+  //std::printf(" NegaAlphaRate: %lg\n", evaluate_nega_alpha(black1, white1, [](auto black, auto white){return evaluate_monte_carlo_full(black, white);}));
+  //std::printf(" NegaAlphaRate: %lg\n", evaluate_nega_alpha(black1, white1, [](auto black, auto white){return evaluate_static(black, white);}));
+  //std::printf(" NegaAlphaRate: %lg\n", evaluate_nega_alpha(black1, white1, [](auto black, auto white){return evaluate_win_loss(black, white);}, 10));
+  //std::printf("MonteCarloRate: %lg\n", evaluate_monte_carlo_random(black1, white1));
   return 0;
 }
